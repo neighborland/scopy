6352853b978bbc632fad4fb395e2eae25d7dcc3a
Tee Parham <tee@neighborland.com>
2015-12-28 17:06:04 -0700

    [style] Whitespace, blank lines

--- lib/scopy/name_scopes.rb
+++ lib/scopy/name_scopes.rb
@@ -1,34 +1,33 @@
 module Scopy
   module NameScopes
     extend ActiveSupport::Concern
-    
+
     # note: case-sensitive like operations do not work as expected in Sqlite,
     # which we are using as the test database.
     # see http://goo.gl/Lxxc4m
-        
+
     included do
-      scope :name_like, ->(text, options={}) do
+      scope :name_like, ->(text, options = {}) do
         cs = options[:case_sensitive]
         where("#{_lhs_name_column(cs)} LIKE ?", "%#{_rhs_name_value(text, cs)}%")
       end
-      
-      scope :name_starts_with, ->(text, options={}) do
+
+      scope :name_starts_with, ->(text, options = {}) do
         cs = options[:case_sensitive]
         where("#{_lhs_name_column(cs)} LIKE ?", "#{_rhs_name_value(text, cs)}%")
-      end      
+      end
     end
-    
-  private
-    
-    module ClassMethods          
+
+    private
+
+    module ClassMethods
       def _lhs_name_column(case_sensitive)
         case_sensitive ? "#{self.table_name}.name" : "LOWER(#{self.table_name}.name)"
       end
-      
+
       def _rhs_name_value(text, case_sensitive)
         case_sensitive ? text : text.try(:downcase)
       end
-    end    
-    
+    end
   end
 end
